b52300a998a2489d20e58f04737b61a9fbe37d43
Maaz Akhter <maa053@edu.zealand.dk>
2026-04-30 11:17:56 +0200

    toppings

--- PizzaStoreLib/Topping.cs
+++ PizzaStoreLib/Topping.cs
@@ -2,34 +2,4 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace PizzaStoreLib
-{
-    public class Topping
-    {
-        private string _navn;
-        private double _pris;
-
-        public Topping(string navn, double pris)
-        {
-            _navn = navn;
-            _pris = pris;
-        }
-
-        public string Navn
-        {
-            get { return _navn; }
-            set { _navn = value; }
-        }
-
-        public double Pris
-        {
-            get { return _pris; }
-            set { _pris = value; }
-        }
-
-        public override string ToString()
-        {
-            return $"{Navn} ({Pris} kr)";
-        }
-    }
-}
+namespace PizzaStoreLib;
